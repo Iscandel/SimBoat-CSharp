15b2bb5af0ceb76c520cf9902f3e96a68ba09088
Iscandel <53024294+Iscandel@users.noreply.github.com>
2022-08-09 02:09:41 +0200

    Added a class for physics constants

---
+++ Assets/UnityPhysicsconstants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UnityEngine;
+
+class UnityPhysicsConstants
+{
+    public static readonly Vector3 G = new Vector3(0, -9.81f, 0);
+
+    public static readonly float RHO = 1026;
+}
