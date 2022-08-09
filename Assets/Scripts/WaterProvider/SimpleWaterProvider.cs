c292b2eae0e0676b93fca0dc85f4377bf96af231
Iscandel <53024294+Iscandel@users.noreply.github.com>
2022-08-09 02:06:52 +0200

    Missing files

---
+++ Assets/Scripts/WaterProvider/SimpleWaterProvider.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+
+internal class SimpleWaterProvider : IWaterProvider
+{
+    private float _waterLevel;
+
+    public SimpleWaterProvider(float height)
+    {
+        _waterLevel = height;
+    }
+
+    public void SetSamplingTime(float time) { }
+
+    public bool SampleHeightAt(Vector3[] samplePoints, ref float[] heights)
+    {
+        for(int i = 0; i < samplePoints.Length; i++) 
+        {
+            heights[i] = _waterLevel;
+        }
+
+        return true;
+    }
+
+    //public float GetHeightAt(int listIndex)
+    //{
+    //    return _waterLevel;
+    //}
+
+    //public bool UpdateSamplingList(Vector3[] samplePoints)
+    //{
+    //    return true;
+    //}
+}
