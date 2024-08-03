92a8c0de5dde8c961e8359e49a79e22971408811
Iscandel <53024294+Iscandel@users.noreply.github.com>
2024-08-03 19:41:17 +0200

    Refactor pause component to be time scale instead

--- Assets/Scripts/Pause.cs
+++ Assets/Scripts/Pause.cs
@@ -7,26 +7,34 @@ public class Pause : MonoBehaviour
     public bool _fixTimeScale = false;
     private bool lastFrameFixTimeScale = false;
     float _previousTimeScale;
-    
+
+    [Tooltip("Angle / North"), Range(0, 2)]
+    public float _timeScale;
+
     // Start is called before the first frame update
     void Start()
     {
-        _previousTimeScale = Time.timeScale;
+        //_previousTimeScale = Time.timeScale;
+
+        _timeScale = Time.timeScale;
+        //Time.fixedDeltaTime = 0.01f;
     }
 
     // Update is called once per frame
     void Update()
     {      
-        if (_fixTimeScale && lastFrameFixTimeScale != _fixTimeScale)
-        {
-            _previousTimeScale = Time.timeScale;
-            Time.timeScale = 0;
-        }
-        else if(!_fixTimeScale && lastFrameFixTimeScale != _fixTimeScale)
-        {
-            Time.timeScale = _previousTimeScale;
-        }
+        //if (_fixTimeScale && lastFrameFixTimeScale != _fixTimeScale)
+        //{
+        //    _previousTimeScale = Time.timeScale;
+        //    Time.timeScale = 0;
+        //}
+        //else if(!_fixTimeScale && lastFrameFixTimeScale != _fixTimeScale)
+        //{
+        //    Time.timeScale = _previousTimeScale;
+        //}
 
-        lastFrameFixTimeScale = _fixTimeScale;
+        //lastFrameFixTimeScale = _fixTimeScale;
+
+        Time.timeScale = _timeScale;
     }
 }
